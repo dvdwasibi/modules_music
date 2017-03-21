a0ac68b422f0ff732041d3b68908c4f413426550
David Yang <dayang@google.com>
2017-03-20 19:30:01 -0700

    Progress
    Change-Id: Icc35e72a1a9ae7cc848ba37e2d315e266c702f97

--- modules/music/lib/src/widgets/playlist.dart
+++ modules/music/lib/src/widgets/playlist.dart
@@ -12,7 +12,6 @@ const double _kBackgroundBannerHeight = 100.0;
 const double _kBannerInnerContentHeight = 50.0;
 
 class PlaylistSurface extends StatelessWidget {
-
   /// The [Playlist] to represent for this [PlaylistSurface]
   final Playlist playlist;
 
@@ -20,39 +19,50 @@ class PlaylistSurface extends StatelessWidget {
   PlaylistSurface({
     Key key,
     @required this.playlist,
-  }) : super(key: key) {
-    assert(playlist!=null);
+  })
+      : super(key: key) {
+    assert(playlist != null);
+  }
+
+  Widget _buildPlaylistDetails() {
+    return new Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        // TODO(dayang@): Get actual duration text
+        new Text('Duration'),
+        // TODO(dayang@): Get actual stuff here
+        new Text('6 days ago'),
+      ],
+    );
   }
 
   Widget _buildHeaderContent() {
-    return new Column(
-      children: <Widget>[
-        new Text(playlist.playlistType),
-        new Expanded(
-          child: new Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              new Text(playlist.title),
-              new Text('6 days ago'),
-            ],
-          ),
+    return new Column(children: <Widget>[
+      new Text(playlist.playlistType),
+      new Expanded(
+        child: new Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            new Text(playlist.title),
+            new Text('6 days ago'),
+          ],
         ),
-        new Expanded(
-          child: new Row(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              new Text('Created by ${playlist.user.username}'),
-              new RaisedButton(
-                child: new Text('Following'),
-                onPressed: ()=>{},
-              ),
-            ],
-          ),
+      ),
+      new Expanded(
+        child: new Row(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            new Text('Created by ${playlist.user.username}'),
+            new RaisedButton(
+              child: new Text('Following'),
+              onPressed: () => {},
+            ),
+          ],
         ),
-      ]
-    );
+      ),
+    ]);
   }
 
   Widget _buildHeader(Color primaryColor) {
@@ -72,9 +82,7 @@ class PlaylistSurface extends StatelessWidget {
     ThemeData theme = Theme.of(context);
     return new Container(
       child: new Stack(
-        children: <Widget>[
-
-        ],
+        children: <Widget>[],
       ),
     );
   }
