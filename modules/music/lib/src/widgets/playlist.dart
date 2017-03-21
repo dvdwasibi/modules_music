1c5a6fe33fe354fe0e3bf9dbbc0ce45e76a988d8
David Yang <dayang@google.com>
2017-03-20 18:46:02 -0700

    in progress
    Change-Id: I499841b129f6cd00ce922fd04c81a8fdc48a37df

---
+++ modules/music/lib/src/widgets/playlist.dart
@@ -0,0 +1,81 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+
+import '../models/playlist.dart';
+
+const double _kTopPadding = 36.0;
+const double _kBackgroundBannerHeight = 100.0;
+const double _kBannerInnerContentHeight = 50.0;
+
+class PlaylistSurface extends StatelessWidget {
+
+  /// The [Playlist] to represent for this [PlaylistSurface]
+  final Playlist playlist;
+
+  /// Constructor
+  PlaylistSurface({
+    Key key,
+    @required this.playlist,
+  }) : super(key: key) {
+    assert(playlist!=null);
+  }
+
+  Widget _buildHeaderContent() {
+    return new Column(
+      children: <Widget>[
+        new Text(playlist.playlistType),
+        new Expanded(
+          child: new Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              new Text(playlist.title),
+              new Text('6 days ago'),
+            ],
+          ),
+        ),
+        new Expanded(
+          child: new Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              new Text('Created by ${playlist.user.username}'),
+              new RaisedButton(
+                child: new Text('Following'),
+                onPressed: ()=>{},
+              ),
+            ],
+          ),
+        ),
+      ]
+    );
+  }
+
+  Widget _buildHeader(Color primaryColor) {
+    return new Container(
+      height: _kBackgroundBannerHeight,
+      color: primaryColor,
+      padding: const EdgeInsets.only(top: _kTopPadding),
+      child: new Container(
+        height: _kBannerInnerContentHeight,
+        child: _buildHeaderContent(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    return new Container(
+      child: new Stack(
+        children: <Widget>[
+
+        ],
+      ),
+    );
+  }
+}
