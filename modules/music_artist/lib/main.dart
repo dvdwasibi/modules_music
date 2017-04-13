e2bd63cb2f7b7a5a122ef77ef5b001f485af2a03
David Yang <dayang@google.com>
2017-04-13 09:45:05 -0700

    In progress
    Change-Id: I1534e9920c1abe5777b698e99dbe55c1d41868e4

---
+++ modules/music_artist/lib/main.dart
@@ -0,0 +1,49 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:application.lib.app.dart/app.dart';
+import 'package:flutter/material.dart';
+import 'package:lib.widgets/modular.dart';
+import 'package:music_widgets/music_widgets.dart';
+
+import 'modular/artist_surface_model.dart';
+
+void main() {
+  ApplicationContext applicationContext =
+      new ApplicationContext.fromStartupInfo();
+
+  // TODO(dayang@): Actually get the ID from the link store
+  ArtistSurfaceModel albumSurfaceModel = new ArtistSurfaceModel(
+    artistId: '6E1rccfBuIsyLUBH81PYoG',
+  );
+
+  ModuleWidget<ArtistSurfaceModel> moduleWidget =
+      new ModuleWidget<ArtistSurfaceModel>(
+    applicationContext: applicationContext,
+    moduleModel: albumSurfaceModel,
+    child: new Scaffold(
+      backgroundColor: Colors.grey[800],
+      body: new SingleChildScrollView(
+        controller: new ScrollController(),
+        child: new ScopedModelDescendant<ArtistSurfaceModel>(builder: (
+          _,
+          __,
+          ArtistSurfaceModel model,
+        ) {
+          return new ArtistSurface(
+            artist: model.artist,
+            albums: model.albums,
+            relatedArtists: model.relatedArtists,
+            loadingStatus: model.loadingStatus,
+            // TODO(dayang@): hook up other actions to real stuff
+          );
+        }),
+      ),
+    ),
+  );
+
+  runApp(moduleWidget);
+  albumSurfaceModel.fetchArtist();
+  moduleWidget.advertise();
+}
