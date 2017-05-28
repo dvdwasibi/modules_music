257521b15aeac40258fec9acf3c20d821aec97b9
David Yang <dayang@google.com>
2017-05-28 14:21:53 -0700

    In progress
    Change-Id: Ia6af4e0d9067237348fe0c419c113656a666925c

---
+++ modules/music_playback/lib/main.dart
@@ -0,0 +1,9 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:application.lib.app.dart/app.dart';
+import 'package:flutter/material.dart';
+import 'package:lib.widgets/modular.dart';
+import 'package:music_models/music_models.dart';
+import 'package:music_widgets/music_widgets.dart';
